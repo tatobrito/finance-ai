067c221d812f8cd046094e5850d35f397f7cc3d2
tatobrito <gnothisway@hotmail.com>
2024-11-25 00:43:09 -0300

    chore: add image setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+const DashboardPage = () => {
+  return <h1>Dashboard page</h1>;
+};
+export default DashboardPage;
---
+++ app/login/page.tsx
@@ -0,0 +1,15 @@
+const LoginPage = () => {
+  return (
+    <div className="grid grid-cols-2 h-full">
+      <div className="relative h-full w-full">
+        <img
+          src="/login.png"
+          alt="Login"
+          className="object-cover w-full h-full"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
