dabae71b5b54c3c8547ea0c3435fdec063373f5f
tatobrito <gnothisway@hotmail.com>
2024-11-24 23:43:24 -0300

    chore: add tailwind pettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex items-center justify-center w-screen">
+      <h1 className="text-red-500 p-5">Home</h1>
     </div>
   );
 };
