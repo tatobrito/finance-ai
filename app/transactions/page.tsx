8c977d9bad7b9157961c1266f645b6acab49bbf9
tatobrito <gnothisway@hotmail.com>
2024-11-30 21:03:49 -0300

    chore: add table trasaction

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,17 @@
-const TransactionsPage = () => {
-    return <h1>Transactions page</h1>;
-}
- 
+import { db } from "@/lib/prisma";
+
+const TransactionsPage = async () => {
+  // acessar as transações do banco
+  const transaction = await db.transaction.findMany({});
+  return (
+    <div className="">
+      {transaction.map((transaction) => (
+        <div key={transaction.id} className="mb-4">
+          {transaction.name}
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default TransactionsPage;
