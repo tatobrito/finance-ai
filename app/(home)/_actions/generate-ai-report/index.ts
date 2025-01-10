ab3977a73db79dab6e92eb0ce67c76b8a3fcbb51
tatobrito <gnothisway@hotmail.com>
2025-01-10 05:52:17 -0300

    feat: add ai report generation

---
+++ app/(home)/_actions/generate-ai-report/index.ts
@@ -0,0 +1,56 @@
+"use server";
+import { db } from "@/_lib/prisma";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import OpenAI from "openai";
+import { generateAiReportSchema, GenerateAiReportSchema } from "./schema";
+
+export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
+  generateAiReportSchema.parse({ month });
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user.publicMetadata.subscriptionPlan == "premium";
+  if (!hasPremiumPlan) {
+    throw new Error("You need a premium plan to generate an AI report");
+  }
+  const openAi = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  // pegar as transações do mês recebido
+  const transactions = await db.transaction.findMany({
+    where: {
+      date: {
+        gte: new Date(`2025-${month}-1`),
+        lt: new Date(`2025-${month}-31`),
+      },
+    },
+  });
+
+  // mandar as transações para o ChatGPT e pedir para ele gerar um relatório com insights
+  const content = `Gere um relatório com insights sobre as minhas finanças, e rientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
+  ${transactions
+    .map(
+      (transaction) =>
+        `${transaction.date.toLocaleDateString("pt-BR")}-R$${transaction.amount}-${transaction.type}-${transaction.category}`,
+    )
+    .join(";")}`;
+  // criando mensagem sistematica
+  const completion = await openAi.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          "Você é um especialista em gestão e organização de finanças pessoais. Você ajuda as pessoas a organizarem melhor as suas finanças.",
+      },
+      {
+        role: "user",
+        content,
+      },
+    ],
+  });
+  // pegar o relatório gerado pelo ChatGPT e retornar para o usuário
+  return completion.choices[0].message.content;
+};
---
+++ app/(home)/_components/ai-report-button.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { DialogContent } from "@/app/_components/ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
+import { DialogFooter, DialogHeader } from "../../_components/ui/dialog";
+import { generateAiReport } from "../_actions/generate-ai-report";
+import { useState } from "react";
+import { ScrollArea } from "@/app/_components/ui/scroll-area";
+import Markdown from "react-markdown";
+import { Loader2Icon } from "lucide-react";
+
+interface AiReportButtonProps {
+  month: string;
+}
+const AiReportButton = ({ month }: AiReportButtonProps) => {
+  const [report, setReport] = useState<string | null>(null);
+  const [reportIsLoading, setReportIsLoading] = useState(false);
+  const handleGenerateReportClick = async () => {
+    setReportIsLoading(true);
+    try {
+      const reportData = await generateAiReport({ month });
+      setReport(reportData);
+    } catch (error) {
+      console.error("Failed to generate AI report:", error);
+    } finally {
+      setReportIsLoading(false);
+    }
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost" className="text-muted-foreground">
+          Relatório IA
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[600px]">
+        <DialogHeader>
+          <DialogTitle>Relatório IA</DialogTitle>
+          <DialogDescription>
+            Use inteligência artificial para gerar um relatório com insights
+            sobre suas finanças.
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="prose max-h-[450px] text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
+          <Markdown>{report}</Markdown>
+        </ScrollArea>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="ghost">Cancelar</Button>
+          </DialogClose>
+          <Button
+            onClick={handleGenerateReportClick}
+            disabled={reportIsLoading}
+          >
+            {reportIsLoading && <Loader2Icon className="animate-spin" />}
+            Gerar relatório
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AiReportButton;
