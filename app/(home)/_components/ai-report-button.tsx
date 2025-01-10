187758a816527bce757f3dd466820249f1c30632
tatobrito <gnothisway@hotmail.com>
2025-01-10 06:15:37 -0300

    feat ensure only premium users can generate ai report

--- app/(home)/_components/ai-report-button.tsx
+++ app/(home)/_components/ai-report-button.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
-import { DialogContent } from "@/app/_components/ui/dialog";
+import { DialogContent, DialogDescription } from "@/app/_components/ui/dialog";
 import {
   Dialog,
   DialogClose,
-  DialogDescription,
   DialogTitle,
   DialogTrigger,
 } from "@radix-ui/react-dialog";
@@ -14,55 +13,87 @@ import { generateAiReport } from "../_actions/generate-ai-report";
 import { useState } from "react";
 import { ScrollArea } from "@/app/_components/ui/scroll-area";
 import Markdown from "react-markdown";
-import { Loader2Icon } from "lucide-react";
+import { BotIcon, Loader2Icon } from "lucide-react";
+import Link from "next/link";
 
 interface AiReportButtonProps {
+  hasPremiumPlan: boolean;
   month: string;
 }
-const AiReportButton = ({ month }: AiReportButtonProps) => {
+
+const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
   const [report, setReport] = useState<string | null>(null);
   const [reportIsLoading, setReportIsLoading] = useState(false);
   const handleGenerateReportClick = async () => {
-    setReportIsLoading(true);
     try {
-      const reportData = await generateAiReport({ month });
-      setReport(reportData);
+      setReportIsLoading(true);
+      const aiReport = await generateAiReport({ month });
+      console.log({ aiReport });
+      setReport(aiReport);
     } catch (error) {
-      console.error("Failed to generate AI report:", error);
+      console.error(error);
     } finally {
       setReportIsLoading(false);
     }
   };
   return (
-    <Dialog>
+    <Dialog
+      onOpenChange={(open) => {
+        if (!open) {
+          setReport(null);
+        }
+      }}
+    >
       <DialogTrigger asChild>
-        <Button variant="ghost" className="text-muted-foreground">
+        <Button variant="ghost">
           Relatório IA
+          <BotIcon />
         </Button>
       </DialogTrigger>
       <DialogContent className="max-w-[600px]">
-        <DialogHeader>
-          <DialogTitle>Relatório IA</DialogTitle>
-          <DialogDescription>
-            Use inteligência artificial para gerar um relatório com insights
-            sobre suas finanças.
-          </DialogDescription>
-        </DialogHeader>
-        <ScrollArea className="prose max-h-[450px] text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
-          <Markdown>{report}</Markdown>
-        </ScrollArea>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="ghost">Cancelar</Button>
-          </DialogClose>
-          <Button
-            onClick={handleGenerateReportClick}
-            disabled={reportIsLoading}
-          >
-            {reportIsLoading && <Loader2Icon className="animate-spin" />}
-            Gerar relatório
-          </Button>
-        </DialogFooter>
+        {hasPremiumPlan ? (
+          <>
+            <DialogHeader>
+              <DialogTitle>Relatório IA</DialogTitle>
+              <DialogDescription>
+                Use inteligência artificial para gerar um relatório com insights
+                sobre suas finanças.
+              </DialogDescription>
+            </DialogHeader>
+            <ScrollArea className="prose max-h-[450px] text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
+              <Markdown>{report}</Markdown>
+            </ScrollArea>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="ghost">Cancelar</Button>
+              </DialogClose>
+              <Button
+                onClick={handleGenerateReportClick}
+                disabled={reportIsLoading}
+              >
+                {reportIsLoading && <Loader2Icon className="animate-spin" />}
+                Gerar relatório
+              </Button>
+            </DialogFooter>
+          </>
+        ) : (
+          <>
+            <DialogHeader>
+              <DialogTitle>Relatório IA</DialogTitle>
+              <DialogDescription>
+                Você precisa de um plano premium para gerar relatórios com IA.
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="ghost">Cancelar</Button>
+              </DialogClose>
+              <Button asChild>
+                <Link href="/subscription">Assinar plano premium</Link>
+              </Button>
+            </DialogFooter>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
@@ -27,6 +27,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   }
   const dashboard = await getDashboard(month);
   const userCanAddTransaction = await canUserAddTransaction();
+  const user = await clerkClient().users.getUser(userId);
 
   return (
     <>
@@ -35,7 +36,10 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <div className="flex items-center gap-4">
-            <AiReportButton month={month} />
+            <AiReportButton
+              month={month}
+              hasPremiumPlan={user.publicMetadata.subscriptionPlan == "premium"}
+            />
             <TimeSelect />
           </div>
         </div>
