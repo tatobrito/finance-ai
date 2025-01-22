e601dc4f385c096731d2425e5e1e98788d5f0455
tatobrito <gnothisway@hotmail.com>
2025-01-22 09:35:04 -0300

    layout com peacock e correção de texto

--- app/(home)/_actions/generate-ai-report/index.ts
+++ app/(home)/_actions/generate-ai-report/index.ts
@@ -29,7 +29,7 @@ export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
   });
 
   // mandar as transações para o ChatGPT e pedir para ele gerar um relatório com insights
-  const content = `Gere um relatório com insights sobre as minhas finanças, e rientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
+  const content = `Gere um relatório com insights sobre as minhas finanças, e orientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
   ${transactions
     .map(
       (transaction) =>
